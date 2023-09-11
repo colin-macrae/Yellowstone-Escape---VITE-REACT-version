b2ae4162456477d8e1561f18236ade8bcc439d1c
Colin Macrae <csmacrae@gmail.com>
2023-09-11 16:14:48 +0800

    initial setup complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// const url = 'https://course-api.com/react-tours-project';
+import NatParksFetch from "./NatParksFetch";
+
+const App = () => {
+  return (
+    <>
+      <h2>Yellowstone Escape</h2>
+      <NatParksFetch />
+    </>
+  );
+};
+export default App;
---
+++ src/NatParksFetch.jsx
@@ -0,0 +1,52 @@
+////// my experiment with nat parks api below
+import { useState, useEffect } from "react";
+
+const NatParksFetch = () => {
+  const [users, setUsers] = useState([]);
+
+  // useEffect is INSIDE a function, not outside
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://developer.nps.gov/api/v1/thingstodo?parkCode=yell&api_key=iSpPR5udcPCzijjVFDgRMe2hLAfepOt9jbFeGFjX"
+        );
+        const users = await response.json();
+        setUsers(users.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // you can put this here but then in the render you must call each variable with "users dot variableName, e.g. users.login"
+  // the alternative is to destructure it INSIDE the map function and each one will already be unique, making the object name (e.g. users.login) unnessecary, and you can do like this, e.g. login.
+  // const { id, node_id, avatar_url, login } = users;
+
+  return (
+    <div>
+      <h2>fetch data example</h2>
+      <div>
+        {users.map((user) => {
+          // in the end i destructured it here instead of outside the map method
+          const { id, title, shortDescription, images, location, season } =
+            user;
+          return (
+            <div key={id}>
+              {/* <img src={user.images.url} alt={"hi"} /> */}
+              <div>{title}</div>
+              <img style={{ width: "550px" }} src={images[0].url} alt={title} />
+              <div>{shortDescription}</div>
+              <div>{location}</div>
+              <div>{season}</div>
+
+              {/* <div>node ID: {node_id}</div> */}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default NatParksFetch;
