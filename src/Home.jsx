eed129af0ceb4813f0924d23eae0532ae646dfd3
Colin Macrae <csmacrae@gmail.com>
2023-09-12 15:17:12 +0800

    home page temporary styling added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,15 @@
 import "./index.css";
 import { Link } from "react-router-dom";
+import "./Home.css"; 
 
 function Home() {
   return (
-    <>
+    <div className="container">
       <main>
-        <h2>Yellowstone Escape</h2>
-        <Link to="/activitypreviews" href="#">See activities</Link>
+        <h1 className="home-header heading-primary">Yellowstone Escape</h1>
+        <Link className="see-activities-btn" to="/activitypreviews" href="#">See activities</Link>
       </main>
-    </>
+    </div>
   );
 }
 export default Home;
