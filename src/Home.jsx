76b04f17932aaeed9d98cd6ea45dd85e2779a4d6
Colin Macrae <csmacrae@gmail.com>
2023-09-13 15:12:52 +0800

    styling added to home button and activity list

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,16 @@
 import "./index.css";
-import { Link } from "react-router-dom";
 import "./Home.css"; 
+import { Link } from "react-router-dom";
+
 
 function Home() {
   return (
     <div className="container">
       <main>
         <h1 className="home-header heading-primary">Yellowstone Escape</h1>
-        <Link className="see-activities-btn" to="/activitypreviews" href="#">See activities</Link>
+        <Link className="see-activities-btn" to="/activitypreviews" href="#">
+          BROWSE ACTIVITIES &rarr;
+        </Link>
       </main>
     </div>
   );
