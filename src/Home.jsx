cae2fea0a25cf2e65b251f25ff8ade927ad2caa5
Colin Macrae <csmacrae@gmail.com>
2023-09-14 13:21:07 +0800

    home screen image and styling added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,26 @@
 import "./index.css";
-import "./Home.css"; 
+import "./Home.css";
 import { Link } from "react-router-dom";
-
 
 function Home() {
   return (
-    <div className="container">
-      <main>
-        <h1 className="home-header heading-primary">Yellowstone Escape</h1>
-        <Link className="see-activities-btn" to="/activitypreviews" href="#">
-          BROWSE ACTIVITIES &rarr;
-        </Link>
-      </main>
+    <div className="container hero-section">
+      <div className="hero-section-text-container">
+        <div>
+          <h1 className="home-header heading-primary">
+            Looking to Escape? Look no Further.
+          </h1>
+          <p>Fifty of Yellowstone Park's most unforgettable activities that you simply must experience.</p>
+          <Link className="see-activities-btn" to="/activitypreviews" href="#">
+            BROWSE ACTIVITIES &rarr;
+          </Link>
+        </div>
+      </div>
+      <img
+        className="#"
+        src="https://www.nps.gov/common/uploads/cropped_image/E0F10DF6-F62B-0546-3A490CD9DCC01051.jpg"
+        alt="yellowstone image"
+      />
     </div>
   );
 }
