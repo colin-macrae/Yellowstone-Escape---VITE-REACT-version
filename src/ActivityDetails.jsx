b731e5ef862cda676cc359b82585388c94a82dd6
Colin Macrae <csmacrae@gmail.com>
2023-10-04 13:44:37 -0700

    activity details fetch & error handling added

--- src/ActivityDetails.jsx
+++ src/ActivityDetails.jsx
@@ -4,9 +4,61 @@ import ActivityPreviews from "./ActivityPreviews";
 
 
 export default function ActivityDetails () {
+  const { id } = useParams();
+  const [activity, setActivity] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function loadActivity(id) {
+      try {
+        setIsLoading(true);
+        const activity = await fetchActivity(id);
+        setActivity(activity);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    loadActivity(id);
+  }, [id]);
+
+  if (isLoading) return <div className="loading-or-error">Loading...</div>;
+  if (error) {
+    return (
+      <div>
+        Error Loading Product {id}: {error.message}
+      </div>
+    );
+  }
+  if (!activity) return null;
+
+  // loop through API's JSON data to find current activity's details
+  let currentActivity = null;
+  function findActivity () {
+    for (let i = 0; i < activity.data.length; i++) {
+      if (activity.data[i].id === id) {
+        currentActivity = activity.data[i];        
+        return currentActivity
+      }
+    }
+  }
+  findActivity()
+
   return (
     <div>
-      Under Construction
+      <p>{currentActivity.title}</p>
+      <p>(under construction)</p>
     </div>
-  )
+  );
+}
+
+async function fetchActivity(id) {
+  const res = await fetch(
+    `https://developer.nps.gov/api/v1/thingstodo?parkCode=yell&api_key=iSpPR5udcPCzijjVFDgRMe2hLAfepOt9jbFeGFjX`
+  );
+  // console.log(res);
+  if (!res.ok) throw new Error(`fetch Error ${res.status}`);
+  return await res.json();  
 }
