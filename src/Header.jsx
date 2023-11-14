02108bba5f50d8c747c5aa692319782fd0456c70
Colin Macrae <csmacrae@gmail.com>
2023-11-13 19:38:34 -0800

    hrefs cleanup

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,19 +5,19 @@ function Header() {
   return (
     <>
       <header className="header">
-        <Link to="/" className="header-title" href="#">
+        <Link to="/" className="header-title">
           <p>Yellowstone Escape</p>
           <p>LOOK NO FURTHER</p>
         </Link>
         <nav className="main-nav">
           <ul>
             <li>
-              <Link to="/activitypreviews" className="nav-link" href="#">
+              <Link to="/activitypreviews" className="nav-link">
                 Browse
               </Link>
             </li>
             <li>
-              <Link to="/myactivities" className="nav-link" href="#">
+              <Link to="/myactivities" className="nav-link">
                 My Activities
               </Link>
             </li>
